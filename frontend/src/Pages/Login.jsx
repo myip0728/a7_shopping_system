34d16fbc221a43ac41f4ba472029d2b9246830bf
chingLo1123 <ching231101@gmail.com>
2024-04-05 02:16:26 +0800

    update login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -8,15 +8,44 @@ export const Login = () => {
     //Testing whether the user wants the password to be visible
     const [visible, setVisible] = useState(false);
 
+    const login = async ()=>{
+        console.log("Login Function Executed", formData);
+        let responseData;
+        await fetch('http://localhost:4000/login',{
+            method:'POST',
+            headers:{
+                Accept:'application/form-data',
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(formData),
+            }).then((response)=>{return response.json()}).then((data)=>responseData=data)
+        if(responseData.success){
+            localStorage.setItem('token',responseData.token);
+            window.location.replace='/'
+        }
+        else{
+            alert(responseData.error)
+        }
+    }
+
+    const [formData, setFormData] = useState({
+        email:"",
+        password:""
+    })
+
+    const changeHandler = (e)  => {
+        setFormData({...formData,[e.target.name]: e.target.value})
+    }
+
     return (
         <div className='loginsignup'>
             <div className="login-container">
                 <h1>Login</h1>
                 <div className='loginsignup-fields'>
-                    <input type="email" placeholder='Eamil Address' />
+                    <input name= 'email' value= {formData.email} onChange = {changeHandler} type="email" placeholder='Eamil Address' />
                 </div>
                 <div className='loginsignup-fields'>
-                    <input type={visible ? "text" : "password"} placeholder='Password' />
+                    <input name= 'password' value= {formData.password} onChange = {changeHandler} type={visible ? "text" : "password"} placeholder='Password' />
                     <div onClick={() => setVisible(!visible)}>
                         {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
                     </div>
