85cc9651d29df69f61f7a734a00c9086a0382853
EstellaLO <117270432+EstellaLO@users.noreply.github.com>
2024-04-05 01:16:35 +0800

    Update CartItems.jsx

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './CartItems.css'
+import { ShopContext } from '../../Context/ShopContext'
 
 const CartItems = () => {
     const {all_products, cartItems, removeFromCart} = useContext(ShopContext);
   return (
     <div className = 'cartitems'>
+        {all_products.map((e) => {
+            if (cartItems[e.id] > 0) {
+                return (
+                    <div className = 'cartitems-format'>
+                        <></>
+                    </div>
+                )
+            }
+        })}
     </div>
   )
 };
