64d0bad9ccd4a8602c8973a070acfc03a2cf3aee
jonaswong1014 <wongjonas2002@gmail.com>
2024-04-06 23:05:27 +0800

    Frontend: Search Function enhancement
    Updated the Search page and algo, to match the project requirement.

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -3,22 +3,102 @@ import './CSS/Search.css';
 import { Link } from 'react-router-dom';
 
 const Search = () => {
-    const [searchKeywords, setSearchKeywords] = useState('');
+    const [searchType, setSearchType] = useState("name");
+    const [searchKeywords, setSearchKeywords] = useState("");
+    const [minPrice, setMinPrice] = useState();
+    const [maxPrice, setMaxPrice] = useState()
 
     const handleKeywordsChange = (event) => {
         setSearchKeywords(event.target.value);
+    };
+
+    const handleSearchTypeChange = (event) => {
+        setSearchType(event.target.value);
+    };
+
+    const handleMinPriceChange = (event) => {
+        setMinPrice(event.target.value);
+    };
+
+    const handleMaxPriceChange = (event) => {
+        setMaxPrice(event.target.value);
     };
 
     return (
         <div className='search'>
             <div className="search-container">
                 <h1>Find what you want</h1>
-                <div className="search-fields">
-                    <input type="text" placeholder="Input keywords here"
-                        value={searchKeywords}
-                        onChange={handleKeywordsChange} />
+                <div className='search-option'>
+                    <p>Search by:</p>
+                    <form>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="name"
+                                checked={searchType === "name"}
+                                onChange={handleSearchTypeChange}
+                            />
+                            Name
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="productId"
+                                onChange={handleSearchTypeChange}
+                            />
+                            Product ID
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="category"
+                                onChange={handleSearchTypeChange}
+                            />
+                            Category
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="priceRange"
+                                onChange={handleSearchTypeChange}
+                            />
+                            Price Range
+                        </label>
+                    </form>
                 </div>
-                <Link to={`/searchresult?keywords=${searchKeywords}`}><button> Search </button></Link>
+                {searchType === "name" || searchType === "category" ?
+                    <div className="search-fields">
+                        <input type="text" placeholder={searchType === "name" ? "Enter Product name here" : "Enter Category here"}
+                            value={searchKeywords}
+                            onChange={handleKeywordsChange} />
+                    </div> : null
+                }
+                {searchType === "productId" ?
+                    <div className="search-fields">
+                        <input type="number" placeholder="Enter Product id here"
+                            value={searchKeywords}
+                            onChange={handleKeywordsChange} />
+                    </div> : null
+                }
+                {searchType === "priceRange" ?
+                    <div className="search-fields-price-range">
+                        <input type="number" placeholder="Minimum Price"
+                            value={minPrice}
+                            min="0"
+                            onChange={handleMinPriceChange} />
+                        <div>To</div>
+                        <input type="number" placeholder="Maximum Price"
+                            value={maxPrice}
+                            min="0"
+                            onChange={handleMaxPriceChange} />
+                    </div> : null
+                }
+
+                <Link to={`/searchresult?searchType=${searchType}&keywords=${searchKeywords}&minPrice=${minPrice}&maxPrice=${maxPrice}`}><button> Search </button></Link>
             </div>
 
         </div>
--- frontend/src/Pages/SearchResult.jsx
+++ frontend/src/Pages/SearchResult.jsx
@@ -3,10 +3,10 @@ import './CSS/SearchResult.css'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Item/Item'
 import { ShopContext } from '../Context/ShopContext';
-import { useSearchParams, Link } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 
-export const SearchResult = (props) => {
+export const SearchResult = () => {
     const { all_product } = useContext(ShopContext);; //using context to receive the product list
     const [numToShow, setNumToShow] = useState(12); //Indicating how many product are showing currently
     const [filteredProducts, setFilteredProducts] = useState([]); //Indicating the Product list the are current showing to the user
@@ -15,27 +15,52 @@ export const SearchResult = (props) => {
 
     const [searchParams] = useSearchParams(); //Getting the Keywords searched from the url
     const old_keywords = searchParams.get("keywords");
+    const old_minPrice = searchParams.get("minPrice");
+    const old_maxPrice = searchParams.get("maxPrice");
+    const old_searchType = searchParams.get("searchType");
+    const [old_searchTypeString, setOldSearchTypeString] = useState("") //For showing search result
+
+
+    //State variable for undergoing a new search
+    const [new_searchType, setNewSearchType] = useState("name");
     const [new_keywords, setNewKeywords] = useState("");
+    const [new_minPrice, setNewMinPrice] = useState();
+    const [new_maxPrice, setNewMaxPrice] = useState();
     const [Result, setResult] = useState([]);
+
+
 
     useEffect(() => { //This ensures that the returned array of product is contains the keywords
         const searchResult = all_product.filter((product) => {
-            const productName = product.name.toLowerCase(); //Getting the product name in all_product
-            const category = product.category.toLowerCase(); //Getting the product category in all_product
-            const tags = product.tag.map((tag) => tag.toLowerCase()); //Getting the product tag in all_product
             const keywords = old_keywords.toLowerCase(); //Getting the keyword we are searching
 
-            return ( //Return the filtered product that meet any one of the conditions
-                productName.includes(keywords) ||
-                category.includes(keywords) ||
-                tags.some((tag) => tag.includes(keywords))
-            );
-        });
+            const productName = product.name.toLowerCase(); //Getting the product name in all_product
+            const category = product.category.toLowerCase(); //Getting the product category in all_product
+            const id = product.id; //Getting the product category in all_product
+            const price = product.new_price;
 
+            switch (old_searchType) {
+                case "name":
+                    setOldSearchTypeString("Product Name");
+                    return (productName.includes(keywords));
+                case "productId":
+                    setOldSearchTypeString("Product Id");
+                    return (id.toString() === keywords);
+                case "category":
+                    setOldSearchTypeString("Category");
+                    return (category === keywords);
+                case "priceRange":
+                    setOldSearchTypeString("Price Range");
+                    return (price >= old_minPrice && price <= old_maxPrice);
+                default:
+                    return all_product;
+            }
+        });
         setResult(searchResult);
         setFilteredProducts(searchResult);
         setNumToShow(Math.min(12, searchResult.length));
-    }, [all_product, old_keywords]);
+        // eslint-disable-next-lin
+    }, [all_product, old_keywords, old_maxPrice, old_minPrice, old_searchType]);
 
 
     useEffect(() => { //This ensure rerendering when the web application switch page in different categories
@@ -69,10 +94,10 @@ export const SearchResult = (props) => {
             default:
                 break;
         }
-
         setFilteredProducts(sortedProducts);
         setNumToShow(Math.min(12, sortedProducts.length));
-    }, [Result, props.category, selectedSortOption]);
+        console.log(sortedProducts);
+    }, [Result, selectedSortOption]);
 
     const productsToShow = filteredProducts.slice(0, numToShow);
 
@@ -95,47 +120,144 @@ export const SearchResult = (props) => {
         setNewKeywords(event.target.value);
     };
 
+    const handleSearchTypeChange = (event) => {
+        setNewSearchType(event.target.value);
+    };
+
+    const handleMinPriceChange = (event) => {
+        setNewMinPrice(event.target.value);
+    };
+
+    const handleMaxPriceChange = (event) => {
+        setNewMaxPrice(event.target.value);
+    };
+
+    const handleButtonClick = () => {
+        if (new_searchType === "priceRange") {
+            setNewKeywords("");
+        } else {
+            setNewMaxPrice();
+            setNewMinPrice();
+        }
+        window.location.href = `/searchresult?searchType=${new_searchType}&keywords=${new_keywords}&minPrice=${new_minPrice}&maxPrice=${new_maxPrice}`
+    }
+
     return (
         <div className='search-result'>
             <div className="searchResult">
-                <div className="search-result-fields">
-                    <input type="text" placeholder="Input keywords here"
-                        value={new_keywords}
-                        onChange={handleKeywordsChange} />
-                    <Link to={`/searchresult?keywords=${new_keywords}`}><button> Search </button></Link>
+                <div className="search-option">
+                    <p>Search again by:</p>
+                    <form>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="name"
+                                checked={new_searchType === "name"}
+                                onChange={handleSearchTypeChange}
+                            />
+                            Name
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="productId"
+                                onChange={handleSearchTypeChange}
+                            />
+                            Product ID
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="category"
+                                onChange={handleSearchTypeChange}
+                            />
+                            Category
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="searchType"
+                                value="priceRange"
+                                onChange={handleSearchTypeChange}
+                            />
+                            Price Range
+                        </label>
+                    </form>
                 </div>
-                <h1>Here is the search result of "{old_keywords}"</h1>
-            </div>
-            <div className="search-result-indexSort">
+                <div className='search-bar-component'>
+                    {new_searchType === "name" || new_searchType === "category" ?
+                        <div className="search-result-fields">
+                            <input type="text" placeholder={new_searchType === "name" ? "Enter Product name here" : "Enter Category here"}
+                                value={new_keywords}
+                                onChange={handleKeywordsChange} />
+                        </div> : null
+                    }
+                    {new_searchType === "productId" ?
+                        <div className="search-result-fields">
+                            <input type="number" placeholder="Enter Product id here"
+                                value={new_keywords}
+                                onChange={handleKeywordsChange} />
+                        </div> : null
+                    }
+                    {new_searchType === "priceRange" ?
+                        <div className="search-result-price-range">
+                            <input type="number" placeholder="Minimum Price"
+                                value={new_minPrice}
+                                min="0"
+                                onChange={handleMinPriceChange} />
+                            <div>To</div>
+                            <input type="number" placeholder="Maximum Price"
+                                value={new_maxPrice}
+                                min="0"
+                                onChange={handleMaxPriceChange} />
+                        </div> : null
+                    }
 
-                <p><span>Showing 1-{Math.min(numToShow, filteredProducts.length)}</span> out of {filteredProducts.length} products</p>
+                    <button onClick={handleButtonClick}> Search </button>
+                </div>
+            </div>
+            {filteredProducts.length !== 0 ?
+                <div>
+                    <h1>Here is the search result of {old_searchType === "priceRange" ? `"${old_searchTypeString}: From ${old_minPrice} to ${old_maxPrice}"` : `${old_searchTypeString}: "${old_keywords}"`}</h1>
 
-                <div className='search-result-sort' >
-                    <div onClick={handleSortby} >Sort by </div>
-                    {!formVisble && <img src={dropdown_icon} alt="" onClick={handleSortby} />}
-                    {formVisble && <form action="">
-                        <select value={selectedSortOption} onChange={handleSortOptionChange}>
-                            <option value="1">highest to lowest rating</option>
-                            <option value="2">lowest to hightest rating</option>
-                            <option value="3">highest to lowest price</option>
-                            <option value="4">lowest to highest price</option>
-                            <option value="5">alphabetical name order</option>
-                            <option value="6">newest to oldest</option>
-                        </select>
-                    </form>}
-                </div>
-            </div>
-            <div className="search-result-products">
-                {productsToShow.map((item, i) => {
-                    return <Item key={i} id={item.id} name={item.name} images={item.images} new_price={item.new_price} old_price={item.old_price} />
-                })}
-            </div>
-            {numToShow < filteredProducts.length && (
-                <div className='search-result-loadmore' onClick={handleLoadMore}>
-                    Explore more
-                </div>
-            )
+                    <div className="search-result-indexSort">
+
+                        <p><span>Showing 1-{Math.min(numToShow, filteredProducts.length)}</span> out of {filteredProducts.length} products</p>
+
+                        <div className='search-result-sort' >
+                            <div onClick={handleSortby} >Sort by </div>
+                            {!formVisble && <img src={dropdown_icon} alt="" onClick={handleSortby} />}
+                            {formVisble && <form action="">
+                                <select value={selectedSortOption} onChange={handleSortOptionChange}>
+                                    <option value="1">highest to lowest rating</option>
+                                    <option value="2">lowest to hightest rating</option>
+                                    <option value="3">highest to lowest price</option>
+                                    <option value="4">lowest to highest price</option>
+                                    <option value="5">alphabetical name order</option>
+                                    <option value="6">newest to oldest</option>
+                                </select>
+                            </form>}
+                        </div>
+                    </div>
+                    <div className="search-result-products">
+                        {productsToShow.map((item, i) => {
+                            return <Item key={i} id={item.id} name={item.name} images={item.images} new_price={item.new_price} old_price={item.old_price} />
+                        })}
+                    </div>
+                    {numToShow < filteredProducts.length && (
+                        <div className='search-result-loadmore' onClick={handleLoadMore}>
+                            Explore more
+                        </div>
+                    )}
+                </div> : null
             }
+            {filteredProducts.length === 0 ?
+                <h1>Sorry, we don't have product that matched the criterion. Please try other search term</h1> : null
+            }
+
         </div >
     )
 }
