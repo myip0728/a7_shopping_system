650de7fa30c571f13760221d24e4ad0494dbc98d
EstellaLO <117270432+EstellaLO@users.noreply.github.com>
2024-04-08 08:47:24 +0800

    Update Checkout.jsx

--- frontend/src/Pages/Checkout.jsx
+++ frontend/src/Pages/Checkout.jsx
@@ -1,4 +1,31 @@
-import 
+import React, { useContext, useState } from 'react'
+import './CSS/Checkout.css'
+import CartItems from '../Components/CartItems/CartItems'
+import { ShopContext } from '../Context/ShopContext'
+import { Cart } from './Cart.jsx'
 
-const checkout = () => {}; 
-export default checkout
+
+const Checkout = () => {
+    const { all_product } = useContext(ShopContext); //Getting all product details
+    const { cartItems, removeitem } = useContext(ShopContext); //Getting Shopping CartItems
+    
+
+    return (
+        <div>
+            {localStorage.getItem('token')
+                ?
+                <div className="checkout-main">
+                    <h1>CHECKOUT</h1>
+                    <div className="cart-container">
+                      ...
+                    </div>
+                </div> :
+                <div className="cart-main-not-login">
+                    <h1>Seems like you haven't login, Let's login to see more</h1>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Cart
