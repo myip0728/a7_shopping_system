c545dbdd955a53601881dacca74720d69f7102bc
jonaswong1014 <wongjonas2002@gmail.com>
2024-04-10 11:44:35 +0800

    Backend: Addproduct fixed
    Now allow admin to upload multiple images. and setting option and option type and tags

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -9,6 +9,11 @@ const AddProduct = () => {
   const [image3, setImage3] = useState(false);
   const [image4, setImage4] = useState(false);
   const [image5, setImage5] = useState(false);
+  const [no_tags, setNo_tags] = useState(0);
+  const [tag, setTag] = useState([]);
+  const [no_opts, setNo_opts] = useState(0);
+  const [optType, setOptType] = useState("");
+  const [option, setOption] = useState([]);
 
   const [productDetails, setProductDetails] = useState({
     name: "",
@@ -16,7 +21,9 @@ const AddProduct = () => {
     category: "headphone",
     new_price: "",
     old_price: "",
-    tag: "",
+    option_type: "",
+    option: [],
+    tag: [],
     no_stock: "",
     short_description: "",
     description: ""
@@ -26,24 +33,48 @@ const AddProduct = () => {
     setImage(e.target.files[0]);
   }
 
-  const imageHandler1 = (e) => {
-    setImage1(e.target.files[0]);
-  }
-
-  const imageHandler2 = (e) => {
-    setImage2(e.target.files[0]);
-  }
-  const imageHandler3 = (e) => {
-    setImage3(e.target.files[0]);
-  }
-  const imageHandler4 = (e) => {
-    setImage4(e.target.files[0]);
-  }
-  const imageHandler5 = (e) => {
-    setImage5(e.target.files[0]);
-  }
   const changeHandler = (e) => {
     setProductDetails({ ...productDetails, [e.target.name]: e.target.value })
+  }
+
+  //Getting Tags input
+  const handleTagChange = (e, index) => {
+    const updatedTag = tag.map((old_tag, i) =>
+      i === index ? e.target.value : old_tag
+    );
+    setTag(updatedTag);
+  }
+
+  const no_tagshandler = (event) => {
+    const inputElement = event.target; // Get the input element
+    const inputValue = parseInt(inputElement.value); // Retrieve the input value as an integer
+    setNo_tags(inputValue);
+    // Create a new array with the desired length and fill it with empty strings
+    const newArray = Array.from({ length: inputValue }, () => "");
+    // Update the state variable with the new array
+    setTag(newArray);
+  };
+
+  //Getting Option input
+  const handleOptChange = (e, index) => {
+    const updatedTag = option.map((old_opt, i) =>
+      i === index ? e.target.value : old_opt
+    );
+    setOption(updatedTag);
+  }
+
+  const no_optionshandler = (event) => {
+    const inputElement = event.target; // Get the input element
+    const inputValue = parseInt(inputElement.value); // Retrieve the input value as an integer
+    setNo_opts(inputValue);
+    // Create a new array with the desired length and fill it with empty strings
+    const newArray = Array.from({ length: inputValue }, () => "");
+    // Update the state variable with the new array
+    setOption(newArray);
+  };
+
+  const handleOptTypeChange = (e) => {
+    setOptType(e.target.value)
   }
 
   const Add_Product = async () => {
@@ -54,7 +85,7 @@ const AddProduct = () => {
     const image_list = [image1, image2, image3, image4, image5];
     const image_urls = [];
 
-
+    //Uploading the image to the server
     for (let i = 0; i < 5; i++) {
       if (image_list[i] !== false) {
         let formData = new FormData();
@@ -76,6 +107,9 @@ const AddProduct = () => {
 
     if (!upload_failureflag) {
       product.images = image_urls;
+      product.tag = tag.slice(0, no_tags);
+      product.option_type = optType;
+      product.option = option.slice(0, no_opts);
       console.log(product);
       await fetch('http://localhost:4000/addproduct', {
         method: 'POST',
@@ -92,46 +126,68 @@ const AddProduct = () => {
 
   return (
     <div className='add-product'>
-      <div className="addproduct-itemfield">
-        <p>Product Name</p>
-        <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Short Description</p>
-        <input value={productDetails.short_description} onChange={changeHandler} type="text" name='short_description' placeholder='Type here' />
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Product Description</p>
-        <input value={productDetails.description} onChange={changeHandler} type="text" name='description' placeholder='Type here' />
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Tag</p>
-        <input value={productDetails.tag} onChange={changeHandler} type="text" name='tag' placeholder='Type here' />
-      </div>
-      <div className="addproduct-price">
+      <div className="addproduct-details">
         <div className="addproduct-itemfield">
-          <p>Price</p>
-          <input value={productDetails.old_price} onChange={changeHandler} type="number" name="old_price" min="1" placeholder='Type here' />
+          <p>Product Name</p>
+          <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
         </div>
         <div className="addproduct-itemfield">
-          <p>Offer Price</p>
-          <input value={productDetails.new_price} onChange={changeHandler} type="number" name="new_price" min="0" placeholder='Type here' />
+          <p>Short Description</p>
+          <input value={productDetails.short_description} onChange={changeHandler} type="text" name='short_description' placeholder='Type here' />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Product Description</p>
+          <input value={productDetails.description} onChange={changeHandler} type="text" name='description' placeholder='Type here' />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>no of tags</p>
+          <input value={no_tags} onChange={no_tagshandler} type="number" name='no_tags' placeholder='Type here' min="0" />
+          {tag.length !== 0 ?
+            <div className='render-variables'>
+              <p>Tags:</p>
+              {tag.map((tag, index) => {
+                return <input value={tag} onChange={(e) => { handleTagChange(e, index) }} id={`tag-${index}`} type="text" name='tag' placeholder='Type here' />
+              })}
+            </div> : null}
+        </div>
+        <div className="addproduct-itemfield">
+          <p>no of option</p>
+          <input value={no_opts} onChange={no_optionshandler} type="number" name='no_opts' placeholder='Type here' min="0" />
+          {option.length !== 0 ?
+            <div className='render-variables'>
+              <p>option type:</p>
+              <input value={optType} onChange={(e) => { handleOptTypeChange(e) }} id="opt_type" type="text" name='option_type' placeholder='Type here' />
+              <p>option:</p>
+              {option.map((opt, index) => {
+                return <input value={opt} onChange={(e) => { handleOptChange(e, index) }} id={`opt-${index}`} type="text" name='option' placeholder='Type here' />
+              })}
+            </div> : null}
+        </div>
+        <div className="addproduct-price">
+          <div className="addproduct-itemfield">
+            <p>Price</p>
+            <input value={productDetails.old_price} onChange={changeHandler} type="number" name="old_price" min="1" placeholder='Type here' />
+          </div>
+          <div className="addproduct-itemfield">
+            <p>Offer Price</p>
+            <input value={productDetails.new_price} onChange={changeHandler} type="number" name="new_price" min="0" placeholder='Type here' />
+          </div>
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Number of stock</p>
+          <input value={productDetails.no_stock} onChange={changeHandler} type="number" name="no_stock" min="1" placeholder='Type here' />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Product Category</p>
+          <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector'>
+            <option value="headphone">headphone</option>
+            <option value="laptop">laptop</option>
+            <option value="keyboard">keyboard</option>
+            <option value="mouse">mouse</option>
+          </select>
         </div>
       </div>
-      <div className="addproduct-itemfield">
-        <p>Number of stock</p>
-        <input value={productDetails.no_stock} onChange={changeHandler} type="number" name="no_stock" min="1" placeholder='Type here' />
-      </div>
-      <div className="addproduct-itemfield">
-        <p>Product Category</p>
-        <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector'>
-          <option value="headphone">headphone</option>
-          <option value="laptop">laptop</option>
-          <option value="keyboard">keyboard</option>
-          <option value="mouse">mouse</option>
-        </select>
-      </div>
-      <div className="addproduct-itemfield">
+      <div className="addproduct-item-image">
         <p>Upload Image</p>
         <label htmlFor="file-input-1">
           <img src={image1 ? URL.createObjectURL(image1) : upload} className='add-product-thumnail-img' alt="" />
